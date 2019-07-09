11d95eb7a275eea16853463720e9ada77da25775
anastasiia.syrykh <syrykh.anastasia@gmail.com>
2019-07-09 18:37:45 +0300

    Fixed login template (done)

--- dev/js/pages/login/login-controller.js
+++ dev/js/pages/login/login-controller.js
@@ -2,4 +2,26 @@ class LoginController {
 	constructor(view) {
 		this.view = view;
 	}
+
+	formValidation() {
+        let loginForm = document.getElementsByClassName('login-form')[0];
+
+        loginForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            let inputUser = document.querySelector('input[name="username"]').value;
+            let inputPass = document.querySelector('input[name="password"]').value;
+            let validation = false;
+            if(inputUser.length > 0 && inputPass.length > 0) validation = window.auth(inputUser, inputPass);
+            if(validation) {
+                LocalStorageData.setToLS('username', inputUser);
+                EventBus.trigger('afterLogin');
+                this.view.destroy();
+            } else {
+                document.querySelector('input[name="username"]').value = '';
+                document.querySelector('input[name="password"]').value = '';
+                this.classList.add('validation-error');
+            }
+        });
+	}
 }
--- dev/js/pages/login/login.js
+++ dev/js/pages/login/login.js
@@ -3,26 +3,10 @@ window.initLogin = function() {
 	let loginController = new LoginController(loginView);
 
 	EventBus.on('renderLogin', function() {
-		loginView.render();
-	});
+        loginView.render();
+    });
 
     EventBus.on('loginRendered', function() {
-        var loginForm = document.getElementsByClassName('login-form')[0];
-
-        loginForm.addEventListener('submit', function(e) {
-            e.preventDefault();
-            let inputUser = document.querySelector('input[name="username"]').value;
-            let inputPass = document.querySelector('input[name="password"]').value;
-            let validation = false;
-            if(inputUser.length > 0 && inputPass.length > 0) validation = window.auth(inputUser, inputPass);
-            if(validation) {
-                LocalStorageData.setToLS('username', inputUser);
-                EventBus.trigger('afterLogin');
-                loginView.destroy();
-            } else {
-                document.getElementById('login').value = '';
-                document.getElementById('pass').value = '';
-            }
-        });
+        loginController.formValidation();
     });
 };
